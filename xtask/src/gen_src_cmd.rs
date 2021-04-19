3678dfc193d19807e0aae051afcae075408d25b0
aki <aki.akaguma@hotmail.com>
2021-04-19 10:34:25 +0900

    version 0.1.7

--- xtask/src/gen_src_cmd.rs
+++ xtask/src/gen_src_cmd.rs
@@ -1,47 +1,20 @@
-use flood_tide_gen::gen_src_match;
-use flood_tide_gen::parse_input_file;
-use flood_tide_gen::update_file;
-use flood_tide_gen::{gen_src_help, SrcHelpFlags};
-use flood_tide_gen::{MetaType, OptStr};
+use flood_tide_gen::{FixupType, MetaType, Pasc};
 
 pub fn do_gen_src() -> anyhow::Result<()> {
-    let (mut vec_optstr, vec_line) = parse_input_file("xtask/src/aki-stats-cmd.txt")?;
-    //
-    fix_type(&mut vec_optstr);
-    //
-    let sss = gen_src_help(&vec_optstr, &vec_line, SrcHelpFlags::default())?;
-    update_file(&sss, "src/conf/cmd.help.rs.txt")?;
-    //
-    let sss = gen_src_match(&vec_optstr)?;
-    update_file(&sss, "src/conf/cmd.match.rs.txt")?;
-    //
-    Ok(())
-}
-
-fn fix_type(vec_optstr: &mut [OptStr]) {
-    for v in vec_optstr {
-        let v_meta_type = match v.lon.as_str() {
-            "head" => MetaType::Usize,
-            "tail" => MetaType::Usize,
-            "locale" => MetaType::Other("opt_locale_loc".to_string()),
-            "query" => MetaType::String,
-            _ => v.meta_type.clone(),
-        };
-        //
-        v.meta_type = v_meta_type;
-        //
-        /*
-        let v_is_vec = match v.lon.as_str() {
-            "expression" => true,
-            "format" => true,
-            _ => false,
-        };
-        v.is_vec = v_is_vec;
-        */
-        let v_is_opt = match v.lon.as_str() {
-            "query" => true,
-            _ => false,
-        };
-        v.is_opt = v_is_opt;
-    }
+    flood_tide_gen::do_gen_src(
+        Pasc::Void,
+        "xtask/src/aki-stats-cmd.txt",
+        Some("src/conf/cmd.help.rs.txt"),
+        Some("src/conf/cmd.match.rs.txt"),
+        |opt_str| {
+            let tup = match opt_str.lon_or_sho() {
+                "head" => (false, false, MetaType::Usize),
+                "tail" => (false, false, MetaType::Usize),
+                "locale" => (false, false, MetaType::Other("opt_locale_loc".into())),
+                "query" => (true, false, opt_str.meta_type.clone()),
+                _ => return None,
+            };
+            Some(FixupType::from_tuple(tup))
+        },
+    )
 }
