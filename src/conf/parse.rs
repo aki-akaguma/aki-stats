1da5e6b676393542d8f9c7095ced0940300cf605
aki <aki.akaguma@hotmail.com>
2026-05-17 16:00:57 +0900

    update crate: flood-tide-gen(0.2.2)

--- src/conf/parse.rs
+++ src/conf/parse.rs
@@ -7,7 +7,6 @@ use flood_tide::{OptParseError, OptParseErrors};
 use crate::util::OptLocaleLoc;
 use crate::util::OptUcXParam;
 use num_format::Locale;
-use std::str::FromStr;
 
 //----------------------------------------------------------------------
 include!("cmd.help.rs.txt");
